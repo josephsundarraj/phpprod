dff8f82aeca82e265f2cd89f526398cf83ea253b
Joseph Sundar Raj <50734892+josephsundarraj@users.noreply.github.com>
2024-05-07 16:10:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 
 <?php
 echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
+echo "Hello world! - This is new release <br>";
+echo "I'm about to learn PHP! - New features added! <br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?> 
 
